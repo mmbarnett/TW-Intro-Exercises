50205acdda30791e3dda08e772466a0ff0c6bdf6
mbarnett51 <mbarnett18@amherst.edu>
2018-06-23 12:00:57 -0400

    Add exercise 1

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,5 +1,11 @@
 public class Exercises {
     public static void main(String[] args) {
-        
+        System.out.println(exc1());
     }
+
+    private static String exc1() {
+        return "*";
+    }
+
+
 }
